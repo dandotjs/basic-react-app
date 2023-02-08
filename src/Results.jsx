7516fbd3770f791e037ef1e4487235fc53e59af6
oxygenplug <dan@dandotjs.com>
2023-02-07 19:48:58 -0600

    added carousel with images to details page. added error boundary, refactored components to use react query and react router, added modal

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,22 +1,27 @@
-import Pet from './Pet';
+import Pet from "./Pet";
 
-const Results = ({pets}) => {
+const Results = ({ pets }) => {
   return (
-    <div className='search'>
-    {!pets.length ? (
-      <h1>No Pets Found</h1>
-    ) : (pets.map(pet => (
-      <Pet
-        animal={pet.animal}
-        name={pet.name}
-        breed={pet.breed}
-        images={pet.images}
-        location={`${pet.city}, ${pet.state}`}
-        key={pet.id}
-      />
-    )))}
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Results;
