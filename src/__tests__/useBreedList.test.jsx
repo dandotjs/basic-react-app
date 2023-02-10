025b63359630781ecf5dcf61f557206b6069b014
oxygenplug <dan@dandotjs.com>
2023-02-09 21:08:46 -0600

    added more tests for useBreedList hook

---
+++ src/__tests__/useBreedList.test.jsx
@@ -0,0 +1,58 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { renderHook, waitFor } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import useBreedList from '../useBreedList';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      staleTimeL: Infinity,
+      retry: false
+    }
+  }
+})
+
+
+test('gives an empty list with no animal provide', async () => {
+  const { result } = renderHook(() => useBreedList(''), {
+    wrapper: ({children}) => (
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    )
+  })
+
+  const [breedList, status] = result.current;
+
+  expect(breedList).toHaveLength(0)
+  expect(status).toBe('loading')
+})
+
+test("gives back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+
+  await waitFor(() => expect(result.current[1]).toBe("success"));
+
+  const [breedList] = result.current;
+  expect(breedList).toEqual(breeds);
+});
